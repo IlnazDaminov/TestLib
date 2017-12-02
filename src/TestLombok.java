00d01867712488b2fd42430df07a7f797bf32b4c
IlnazDamiov <ilnaz.daminov@bk.ru>
2017-12-02 15:46:25 +0300

    init commit

---
+++ src/TestLombok.java
@@ -0,0 +1,16 @@
+import lombok.Getter;
+import lombok.extern.java.Log;
+
+import java.util.logging.Level;
+
+@Log
+public class TestLombok{
+    @Getter
+    private int a;
+
+    private int b;
+
+    public void hiLog(){
+        log.log(Level.WARNING,"Hello from loger");
+    }
+}
